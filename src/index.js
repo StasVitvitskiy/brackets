6fa64f6bc01c95ad7aac4604fcf4c09453990e78
StasVitvitskiy <stanislav.i.vitvitskiy@gmail.com>
2020-03-04 12:59:41 -0800

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let result = [];
+    for(let i = 0; i < str.length; i++) {
+        let found = bracketsConfig.find((el) => {return el[0] === str[i] || el[1] === str[i]});
+        if(str[i] === found[1]) {
+            if(found[0] === result[result.length-1]) {
+                result.length = result.length-1;
+            } else {
+                result.push(str[i]);
+            }
+        } else {
+            result.push(str[i]);
+        }
+    }
+    return result.length === 0;
 }
